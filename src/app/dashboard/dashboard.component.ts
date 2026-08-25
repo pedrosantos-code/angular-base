0e25889ddb5c2ea1ac821e65460fe47926b9b40c
PedroPereiradosSantos <ppedropds@gmail.com>
2026-08-25 17:56:50 -0300

    uncomment imports for RouterLink in dashboard and home components.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  // imports: [RouterLink],
+  imports: [RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
