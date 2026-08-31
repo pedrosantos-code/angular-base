d66b26f696a49176565feebde76f5d3bfcace69d
PedroPereiradosSantos <ppedropds@gmail.com>
2026-08-31 13:57:42 -0300

    Refactor code structure for improved readability and maintainability.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,11 +7,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-
 export class DashboardComponent {
   protected readonly title = signal('meu-projeto');
 
+  // Declaração dos Signals
   menuAberto = signal<string | null>(null);
+  sidebarAberta = signal<boolean>(false); 
 
   toggleMenu(nomeMenu: string): void {
     if (this.menuAberto() === nomeMenu) {
@@ -23,5 +24,15 @@ export class DashboardComponent {
 
   fecharMenus(): void {
     this.menuAberto.set(null);
+  }
+
+  // Métodos da Barra Lateral (Sidebar)
+  abrirSidebar(): void {
+    this.sidebarAberta.set(true);
+    this.fecharMenus(); // Fecha o dropdown ao abrir a barra
+  }
+
+  fecharSidebar(): void {
+    this.sidebarAberta.set(false);
   }
 }
