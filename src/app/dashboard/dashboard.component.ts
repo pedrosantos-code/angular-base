d3f1673bb663686894a1c3100c59bfa38d5365f7
PedroPereiradosSantos <ppedropds@gmail.com>
2026-08-31 10:41:46 -0300

    implement dropdown menus for user profile and settings in dashboard and home components; add toggle functionality

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+
+export class HomeComponent {
   protected readonly title = signal('meu-projeto');
+
+  menuAberto = signal<string | null>(null);
+
+  toggleMenu(nomeMenu: string): void {
+    if (this.menuAberto() === nomeMenu) {
+      this.menuAberto.set(null); 
+    } else {
+      this.menuAberto.set(nomeMenu); 
+    }
+  }
+
+  fecharMenus(): void {
+    this.menuAberto.set(null);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
+
 export class HomeComponent {
   protected readonly title = signal('meu-projeto');
+
+  menuAberto = signal<string | null>(null);
+
+  toggleMenu(nomeMenu: string): void {
+    if (this.menuAberto() === nomeMenu) {
+      this.menuAberto.set(null); 
+    } else {
+      this.menuAberto.set(nomeMenu); 
+    }
+  }
+
+  fecharMenus(): void {
+    this.menuAberto.set(null);
+  }
 }
