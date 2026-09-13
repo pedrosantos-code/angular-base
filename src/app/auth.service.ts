e2a3a764656f3ceac725abf39bfc064090a7f7c8
PedroPereiradosSantos <ppedropds@gmail.com>
2026-09-13 17:52:42 -0300

    fix: correct environment import path in auth.service.ts.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthResponse, createClient } from '@supabase/supabase-js'
-import { environment } from '../environments/environments';
+import { environment } from '../environments/environment';
 import { Observable, from, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
